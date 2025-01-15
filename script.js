c019b3438241f16d81cf22dd825ba5e5a83286fb
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-01-15 15:43:11 +0530

    Com

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+const images = document.querySelectorAll('.image')
+let draggedElement = null;
+
+images.forEach((image)=>{
+    image.addEventListener('dragstart', (e)=>{
+        draggedElement = e.target;
+        e.target.style.opacity = '0.5';
+  
+})
+
+image.addEventListener('dragend', (e)=>{
+    e.target.style.opacity = '1';
+    draggedElement = null;
+});
+
+image.addEventListener('dragover', (e)=>{
+    e.preventDefault();
+});
+
+image.addEventListener('drop', (e)=>{
+    e.preventDefault();
+
+    if(draggedElement && draggedElement !== e.target){
+
+        const draggedBackground = draggedElement.style.backgroundImage;
+        e.target.style.backgroundImage = draggedBackground;
+    }
+})
+})
